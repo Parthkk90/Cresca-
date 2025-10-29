a9cd48688cd4efc2022c55b944c7de557cf6ba16
parthkk <parth122004@gmail.com>
2025-10-29 21:30:29 +0530

    Setup wallet app

---
+++ app/index.js
@@ -0,0 +1,10 @@
+/**
+ * AptPays Super Wallet
+ * React Native Entry Point
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
